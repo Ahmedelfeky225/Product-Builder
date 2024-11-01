38a85e752603bafab1254320fbbd4e62ce3ebf7c
Ahmed <elfekyahmed337@gmail.com>
2024-11-01 05:23:13 +0200

    04-Tailwind-css-basics

---
+++ src/components/ProductCards.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ProductCards = (props: Props) => {
+  return (
+    <div className='p-3 border border-white text-lime-50 text-sm rounded-md'>ProductCards</div>
+  )
+}
+
+export default ProductCards
