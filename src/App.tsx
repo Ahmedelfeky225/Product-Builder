97a5c7df6f5fe67f9ba5f16668321f1a8b7f4508
Ahmed <elfekyahmed337@gmail.com>
2024-11-01 06:50:50 +0200

    05-Tailwind-CSS-Mobile-First-Rule

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ProductCards from './components/ProductCards'
 const App = () => {
   return (
     <div>
-     <div className='border-2 border-red-600 grid grid-cols-4 m-6 p-1 bg-red-400 rounded-md gap-3'>
+     <div className='border-2 border-red-600 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m-6 p-1 bg-red-400 rounded-md gap-3'>
      <ProductCards/>
       <ProductCards/>
       <ProductCards/>
