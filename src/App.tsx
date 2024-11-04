2f22698dd755c4d82f6159b963071b21613ef93c
Ahmed <elfekyahmed337@gmail.com>
2024-11-04 16:55:43 +0200

    17-Modal-Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import ProductCards from './components/ProductCards'
+import Modal from './components/ui/Modal'
 import { productList } from './data'
 
 //** */ Render
@@ -9,6 +10,7 @@ const App = () => {
      <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m-6 gap-2 md:gap-4'>
       {RenderList}
      </div>
+     <Modal/>
     </div>
   )
 }
